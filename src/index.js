4f62c4e86927af88f519bfc67e8b0a72daa4bd90
Joseph Martin <josephmartin1350@gmail.com>
2021-12-08 13:15:22 -0700

    commit tree 469048199d8beb3050ce4f5754de657560c87fce

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
+// Add event listeners to capture form data and override a form's default behavior
+// Fetch data based on what the user types into that form
+// Display that data on the page
+
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        // console.log(event.target.children[1].value);
+        const input = document.querySelector('input#searchByID');
+        // console.log(input.value)
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(r => r.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        })
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
